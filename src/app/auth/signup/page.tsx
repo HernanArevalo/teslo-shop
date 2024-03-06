574b790d287b3214546141b5e12428483a0b0362
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-03-06 00:21:13 -0300

    Style login and signup pages

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,10 +1,44 @@
-import Image from 'next/image'
+import { titleFont } from '@/config/fonts';
+import Link from 'next/link';
 
 export default function SignupPage() {
   return (
-    <main className="">
-      <h1>SignupPage</h1>
-      <h1>Hola mundo</h1>
-    </main>
-  )
+    <div className="flex flex-col min-h-screen pt-32 sm:pt-52">
+
+      <h1 className={ `${ titleFont.className } text-4xl mb-5` }>Sign up</h1>
+
+      <div className="flex flex-col">
+
+        <label htmlFor="email">Full name</label>
+        <input
+          className="px-5 py-2 border bg-gray-200 rounded mb-5"
+          type="text" />
+
+        <label htmlFor="email">Password</label>
+        <input
+          className="px-5 py-2 border bg-gray-200 rounded mb-5"
+          type="email" />
+
+        <button
+          className="btn-primary">
+          Sign up
+        </button>
+
+
+        {/* divisor l ine */ }
+        <div className="flex items-center my-5">
+          <div className="flex-1 border-t border-gray-500"></div>
+          <div className="px-2 text-gray-800">or</div>
+          <div className="flex-1 border-t border-gray-500"></div>
+        </div>
+
+        <Link
+          href="/auth/login" 
+          className="btn-secondary text-center">
+          Log in 
+        </Link>
+
+      </div>
+    </div>
+  );
 }
