dae90a91ce0329b097c04675604aebd711f4b668
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-03-07 21:09:12 -0300

    Create the seed database

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -1,14 +1,69 @@
 import { initialData } from "./seed";
+import prisma from '../lib/prisma'
+
 
 async function main() {
 
+    const { categories, products } = initialData
 
-    console.log(initialData);
+    // 1. detele previous records
+    await prisma.productImage.deleteMany(),
+    await prisma.product.deleteMany(),
+    await prisma.category.deleteMany()
+
+    // categories
+    const categoriesData = categories.map((name) => (
+        { name: name.charAt(0).toUpperCase() + name.slice(1) }
+    ));
+
+    await prisma.category.createMany({
+        data: categoriesData
+    });
+
+    const categoriesDB = await prisma.category.findMany()
+    
+    const categoriesMap = categoriesDB.reduce((map, category) =>{
+        map[category.name.toLowerCase()] = category.id
+
+        return map
+    }, {} as Record<string,string>)
+
+
+
+    // products
+    products.forEach( async(product) => {
+        const {images, type, ...rest} = product;
+
+        const dbProduct = await prisma.product.create({
+            data: {
+                ...rest,
+                categoryId: categoriesMap[type.toLowerCase()]
+            }
+        })
+
+        
+        // images 
+        const imagesData = images.map(image => ({
+            url: image,
+            productId: dbProduct.id
+        }))
+
+        await prisma.productImage.createMany({
+            data: imagesData
+        })
+
+    });
+
 
 
 
     console.log('seed executed properly');
 }
+
+
+
+
+
 
 (() => {
     if(process.env.NODE_ENV === 'production') return;
