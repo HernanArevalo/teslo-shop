6e6d328f44fccd53954b24a98b211c659eb5982f
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-03-11 18:34:08 -0300

    Start manual pagination

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,10 +1,18 @@
 import { getPaginatedProductsWithImages } from '@/actions';
 import { ProductsGrid, Title } from '@/components';
 
+interface Props {
+  searchParams: {
+    page?: string
+  }
+}
 
-export default async function Home() {
 
-  const {products} = await getPaginatedProductsWithImages();
+export default async function Home({ searchParams  }: Props) {
+
+  const page = searchParams.page ? parseInt( searchParams.page ) : 1;
+
+  const {products} = await getPaginatedProductsWithImages({page});
 
   return (
     <>
