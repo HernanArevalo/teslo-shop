633e3f318e87187b34731d903fe1c63e618deed3
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-04-29 19:10:37 -0300

    Get and Delete user address actions

---
+++ src/actions/address/delete-user-address.ts
@@ -0,0 +1,29 @@
+'use server';
+
+import prisma from "@/lib/prisma";
+
+export const deleteUserAddress = async(userId:string) => {
+
+  try {
+
+    const deletedAdress = await prisma.userAddress.delete({
+      where: {
+        userId: userId
+      }
+    })
+
+    return {
+      ok: true,
+      deletedAdress: deletedAdress
+    }
+  } catch (error) {
+    console.log(error);
+
+    return {
+      ok: false,
+      message: 'Error deleting userAddres'
+    }
+  }
+
+
+}
