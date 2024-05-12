bd3497b2f0bc00e98cb22c793c26b078d7045aef
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-05-12 17:21:38 -0300

    place order in database and start transaction system

--- src/actions/order/place-order.ts
+++ src/actions/order/place-order.ts
@@ -1,29 +1,99 @@
 'use server';
 
-import { auth } from "@/auth.config";
-import type { Address, Size } from "@/interfaces";
+import { auth } from '@/auth.config';
+import type { Address, Size } from '@/interfaces';
+import prisma from '@/lib/prisma';
 
-interface ProductToOrder{
+interface ProductToOrder {
   productId: string;
   quantity: number;
-  size: Size
+  size: Size;
 }
 
-export const placeOrder = async( productsIds: ProductToOrder[], address: Address) =>{
-
+export const placeOrder = async (
+  productsIds: ProductToOrder[],
+  address: Address
+) => {
   const session = await auth();
-
   const userId = session?.user.id;
 
-  if ( !userId ) {
-    return{
+  // check user session
+  if (!userId) {
+    return {
       ok: false,
-      message: 'No hay sesión de usuario'
-    }
-
-    
+      message: 'No hay sesión de usuario',
+    };
   }
 
-  console.log({ productsIds, address, userId });
+  const products = await prisma.product.findMany({
+    where: {
+      id: {
+        in: productsIds.map((p) => p.productId),
+      },
+    },
+  });
 
-}
+  // calculate amount
+  const itemsInOrder = productsIds.reduce((count, p) => count + p.quantity, 0);
+
+  // totals: tax, subtotal, and total
+  const { subtotal, tax, total } = productsIds.reduce(
+    (totals, item) => {
+      const productQuantity = item.quantity;
+      const product = products.find((p) => p.id === item.productId);
+
+      if (!product) throw new Error(`${item.productId} no existe - 500`);
+
+      const subtotal = product.price * productQuantity;
+
+      totals.subtotal += subtotal;
+      totals.tax += subtotal * 0.15;
+      totals.total += subtotal * 1.15;
+
+      return totals;
+    },
+    { subtotal: 0, tax: 0, total: 0 }
+  );
+
+  // create transaction
+
+  const prismaTx = await prisma.$transaction(async (tx) => {
+    // 1. update products stock
+
+    // 2. create order - header - details
+    const order = await tx.order.create({
+      data: {
+        userId: userId,
+        items: itemsInOrder,
+        subTotal: subtotal,
+        tax: tax,
+        total: total,
+        OrderItems: {
+          createMany: {
+            data: productsIds.map((p) => ({
+              quantity: p.quantity,
+              size: p.size,
+              productId: p.productId,
+              price:
+                products.find((product) => product.id === p.productId)?.price ??
+                0
+            }))
+          }
+        }
+      }
+    });
+
+    // Validate prize zero, and send error 
+
+    // 3. create order address
+
+    return {
+      ok: true,
+      order: order,
+      updatedProducts: [],
+      OrderAddress: {}
+    };
+  });
+
+  console.log(prismaTx);
+};
