b3f87923c12d003c4bc4878a17707674be7deb4f
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-05-15 14:58:24 -0300

    add order actions

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -10,6 +10,8 @@ export * from './auth/register'
 export * from './country/getCountries'
 
 export * from './order/place-order'
+export * from './order/get-order'
+export * from './order/get-orders-by-user'
 
 export * from './product/get-product-by-slug'
 export * from './product/get-stock-by-slug'
