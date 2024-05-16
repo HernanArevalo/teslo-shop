f34e285ed6c1d68049de4d0e7c4b77a00337b38d
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-05-16 12:10:18 -0300

    get order from database

--- src/app/(shop)/orders/[id]/page.tsx
+++ src/app/(shop)/orders/[id]/page.tsx
@@ -1,87 +1,98 @@
-import { QuantitySelector, Title } from "@/components";
-import { initialData } from "@/seed/seed";
-import clsx from "clsx";
-import Image from "next/image";
-import Link from "next/link";
-import { IoCartOutline } from "react-icons/io5";
+import { redirect } from 'next/navigation';
+import Image from 'next/image';
 
+import clsx from 'clsx';
+import { IoCartOutline } from 'react-icons/io5';
 
-const productsInCart = [
-  initialData.products[0],
-  initialData.products[1],
-  initialData.products[2],
-]
+import { getOrderById } from '@/actions';
+import { Title } from '@/components';
+import { currencyFormat } from '@/utils';
+
 
 interface Props {
   params: {
-    id: string
-  }
+    id: string;
+  };
 }
 
-export default function OrderPage({ params }: Props ) {
+export default async function OrderPage({ params }: Props) {
+  const { id } = params;
 
-  const { id } = params 
+  const { ok, order } = await getOrderById(id);
 
-  // TODO id verification
-  // redirect()
+  if (!ok) {
+    redirect('/');
+  }
+
+  const address = order?.OrderAddress;
 
   return (
     <div className="flex justify-center items-start mb-72 px-10 sm:px-10">
-      
       <div className="flex flex-col w-[1000px]">
-        <Title title={`Order no. ${id}`}/>
+        <Title title={`Order #${id.split('-').at(-1)}`} />
 
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
           {/* Cart */}
           <div className="flex flex-col mt-5">
-            <div className={clsx(
-              "flex items-center gap-4 rounded-lg py-2 px-3.5 text-xs font-bold text-white mb-5",
-              {
-                "bg-red-600": false,
-                "bg-green-600": true
-              }
-            )}>
-              <IoCartOutline size={30}/>
+            <div
+              className={clsx(
+                'flex items-center gap-4 rounded-lg py-2 px-3.5 text-xs font-bold text-white mb-5',
+                {
+                  'bg-red-600': !order?.isPaid,
+                  'bg-green-600': order?.isPaid,
+                }
+              )}
+            >
+              <IoCartOutline size={30} />
               {/* <span className="mx-2">Pendiente</span> */}
-              <span className="mx-2">Pagado</span>
+              <span className="mx-2">
+                {order?.isPaid ? 'Pagado' : 'No pagado'}
+              </span>
             </div>
 
-          { productsInCart.map( product => (
-            <div className="flex gap-5 mb-2" key={product.slug}>
-              <Image
-                src={`/products/${ product.images[0]}`}
-                width={ 100 }
-                height={ 100 }
-                alt={ product.title}
-                className="rounded"
-              />
-              <div>
-                <p>{product.title}</p>
-                <p>{product.price} x 3</p>
-                <p className="font-bold">Subtotal: ${ product.price * 3 }</p>
+            {order?.OrderItems.map((item) => (
+              <div
+                className="flex gap-5 mb-2"
+                key={item.product.slug + '-' + item.size}
+              >
+                <Image
+                  src={`/products/${item.product.ProductImage[0].url}`}
+                  width={100}
+                  height={100}
+                  alt={item.product.title}
+                  className="rounded"
+                />
+                <div>
+                  <p>
+                    {item.product.title} - {item.size}
+                  </p>
+                  <p>
+                    {item.price} x {item.quantity}
+                  </p>
+                  <p className="font-bold">
+                    Subtotal: {currencyFormat(item.price * item.quantity)}
+                  </p>
 
-                <button className="underline mt-1">Delete</button>
+                  <button className="underline mt-1">Delete</button>
+                </div>
               </div>
-              
-            </div>
-          ))
-
-          }
+            ))}
           </div>
-
-
 
           {/* Checkout */}
           <div className="bg-white rounded-xl shadow-xl p-7 h-fit">
-
             <h2 className="text-2xl mb-2">Adress Delivry</h2>
             <div className="mb-10">
-              <p>Fernando Herrera</p>
-              <p>Av. Siempre viva 123</p>
-              <p>Col. Centro</p>
-              <p>Alcaldía Cuauhtémoc</p>
-              <p>CP 6565</p>
-              <p>Tel 54 35 545 765</p>
+              <p className="text-xl">
+                {address?.firstName} {address?.lastName}
+              </p>
+              <p>{address?.address}</p>
+              <p>{address?.address2}</p>
+              <p>{address?.postalCode}</p>
+              <p>
+                {address?.city}, {address?.countryId}
+              </p>
+              <p>{address?.phone}</p>
             </div>
 
             {/* Divider */}
@@ -90,42 +101,43 @@ export default function OrderPage({ params }: Props ) {
             <h2 className="text-2xl mb-2">Order resume</h2>
 
             <div className="grid grid-cols-2">
+              <span className="font-semibold">Products</span>
+              <span className="text-right">
+                {order?.items !== 1 ? `${order?.items} artcicles` : `1 article`}
+              </span>
 
-              <span>Products</span>
-              <span className="text-right">3 articles</span>
+              <span className="font-semibold">Subtotal</span>
+              <span className="text-right">{currencyFormat(order!.subTotal)}</span>
 
-              <span>Subtotal</span>
-              <span className="text-right">$ 100</span>
+              <span className="font-semibold">Taxes (15%)</span>
+              <span className="text-right">{currencyFormat(order!.tax)}</span>
 
-              <span>Taxes (15%)</span>
-              <span className="text-right">$ 100</span>
-
-              <span className="mt-5 text-2xl">Total:</span>
-              <span className="mt-5 text-2xl text-right">$ 100</span>
-
+              <span className="mt-5 text-2xl font-semibold">Total:</span>
+              <span className="mt-5 text-2xl text-right">
+                {currencyFormat(order!.total)}
+              </span>
             </div>
 
             <div className="mt-5 mb-2 w-full">
-
-              <div className={clsx(
-                "flex items-center gap-4 rounded-lg py-2 px-3.5 text-xs font-bold text-white mb-5",
-                {
-                  "bg-red-600": false,
-                  "bg-green-600": true
-                }
-              )}>
-                <IoCartOutline size={30}/>
+              <div
+                className={clsx(
+                  'flex items-center gap-4 rounded-lg py-2 px-3.5 text-xs font-bold text-white mb-5',
+                  {
+                    'bg-red-600': !order?.isPaid,
+                    'bg-green-600': order?.isPaid,
+                  }
+                )}
+              >
+                <IoCartOutline size={30} />
                 {/* <span className="mx-2">Pendiente</span> */}
-                <span className="mx-2">Pagado</span>
+                <span className="mx-2">
+                  {order?.isPaid ? 'Pagado' : 'No pagado'}
+                </span>
               </div>
-
             </div>
-
           </div>
-
         </div>
       </div>
-
     </div>
   );
 }
