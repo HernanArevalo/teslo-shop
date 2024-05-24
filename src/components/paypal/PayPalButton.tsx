6db0bf3c58a217759ea4b83a9876c4f7dbc20751
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-05-24 14:59:48 -0300

    format document

--- src/components/paypal/PayPalButton.tsx
+++ src/components/paypal/PayPalButton.tsx
@@ -1,20 +1,23 @@
-'use client'
+'use client';
 
-import { PayPalButtons, usePayPalScriptReducer } from '@paypal/react-paypal-js'
-import { CreateOrderData, CreateOrderActions, OnApproveData, OnApproveActions } from '@paypal/paypal-js'
+import { PayPalButtons, usePayPalScriptReducer } from '@paypal/react-paypal-js';
+import {
+  CreateOrderData,
+  CreateOrderActions,
+  OnApproveData,
+  OnApproveActions,
+} from '@paypal/paypal-js';
 import { paypalCheckPayment, setTransactionId } from '@/actions';
 
 interface Props {
   orderId: string;
-  amount: number
+  amount: number;
 }
 
-export const PayPalButton = ({orderId, amount}: Props ) => {
+export const PayPalButton = ({ orderId, amount }: Props) => {
+  const [{ isPending }] = usePayPalScriptReducer();
 
-  const [{ isPending }] = usePayPalScriptReducer()
-
-  const roundedAmount = (Math.round(amount * 100)) / 100
-
+  const roundedAmount = Math.round(amount * 100) / 100;
 
   if (isPending) {
     return (
@@ -22,51 +25,43 @@ export const PayPalButton = ({orderId, amount}: Props ) => {
         <div className="h-11 bg-gray-300 rounded"></div>
         <div className="h-11 bg-gray-300 rounded mt-3"></div>
       </div>
-    )
+    );
   }
 
-  const createOrder = async(data: CreateOrderData, actions: CreateOrderActions): Promise<string> => {
-
+  const createOrder = async (
+    data: CreateOrderData,
+    actions: CreateOrderActions
+  ): Promise<string> => {
     const transactionId = await actions.order.create({
       intent: 'CAPTURE',
       purchase_units: [
-
         {
-          // invoice_id ó reference_id: 'order_id'
+          invoice_id: orderId,
+          //reference_id: 'order_id'
           amount: {
             value: roundedAmount.toString(),
             currency_code: 'USD',
-            
-          }
-        }
-      ]
-    })
+          },
+        },
+      ],
+    });
 
     // console.log({transactionId});
-    const { ok } = await setTransactionId(orderId, transactionId)
-    if ( !ok){
-      throw new Error(`No se pudo actualizar la orden`)
+    const { ok } = await setTransactionId(orderId, transactionId);
+    if (!ok) {
+      throw new Error(`No se pudo actualizar la orden`);
     }
 
-    return transactionId
-  }
+    return transactionId;
+  };
 
-  const onApprove = async(data: OnApproveData, actions: OnApproveActions) => {
-
+  const onApprove = async (data: OnApproveData, actions: OnApproveActions) => {
     const details = await actions.order?.capture();
 
-    if (!details) return
+    if (!details) return;
 
-    await paypalCheckPayment( details.id ?? '' )
-  }
+    await paypalCheckPayment(details.id ?? '');
+  };
 
-
-
-
-  return (
-    <PayPalButtons 
-      createOrder={ createOrder }
-      onApprove={ onApprove }
-    />
-  )
-}
+  return <PayPalButtons createOrder={createOrder} onApprove={onApprove} className="relative z-0"/>;
+};
