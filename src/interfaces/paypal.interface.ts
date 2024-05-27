0eea2d24f26514916f4f427cf7b23134d5ef042d
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-05-26 22:23:37 -0300

    format document

--- src/interfaces/paypal.interface.ts
+++ src/interfaces/paypal.interface.ts
@@ -1,26 +1,26 @@
 export interface PayPalOrderStatusResponse {
-  id:             string;
-  intent:         string;
-  status:         string;
+  id: string;
+  intent: string;
+  status: string;
   payment_source: PaymentSource;
   purchase_units: PurchaseUnit[];
-  payer:          Payer;
-  create_time:    Date;
-  update_time:    Date;
-  links:          Link[];
+  payer: Payer;
+  create_time: Date;
+  update_time: Date;
+  links: Link[];
 }
 
 export interface Link {
-  href:   string;
-  rel:    string;
+  href: string;
+  rel: string;
   method: string;
 }
 
 export interface Payer {
-  name:          PayerName;
+  name: PayerName;
   email_address: string;
-  payer_id:      string;
-  address:       PayerAddress;
+  payer_id: string;
+  address: PayerAddress;
 }
 
 export interface PayerAddress {
@@ -29,7 +29,7 @@ export interface PayerAddress {
 
 export interface PayerName {
   given_name: string;
-  surname:    string;
+  surname: string;
 }
 
 export interface PaymentSource {
@@ -37,30 +37,30 @@ export interface PaymentSource {
 }
 
 export interface Paypal {
-  email_address:  string;
-  account_id:     string;
+  email_address: string;
+  account_id: string;
   account_status: string;
-  name:           PayerName;
-  address:        PayerAddress;
+  name: PayerName;
+  address: PayerAddress;
 }
 
 export interface PurchaseUnit {
   reference_id: string;
-  amount:       Amount;
-  payee:        Payee;
-  invoice_id:   string;
-  shipping:     Shipping;
-  payments:     Payments;
+  amount: Amount;
+  payee: Payee;
+  invoice_id: string;
+  shipping: Shipping;
+  payments: Payments;
 }
 
 export interface Amount {
   currency_code: string;
-  value:         string;
+  value: string;
 }
 
 export interface Payee {
   email_address: string;
-  merchant_id:   string;
+  merchant_id: string;
 }
 
 export interface Payments {
@@ -68,40 +68,40 @@ export interface Payments {
 }
 
 export interface Capture {
-  id:                          string;
-  status:                      string;
-  amount:                      Amount;
-  final_capture:               boolean;
-  seller_protection:           SellerProtection;
+  id: string;
+  status: string;
+  amount: Amount;
+  final_capture: boolean;
+  seller_protection: SellerProtection;
   seller_receivable_breakdown: SellerReceivableBreakdown;
-  invoice_id:                  string;
-  links:                       Link[];
-  create_time:                 Date;
-  update_time:                 Date;
+  invoice_id: string;
+  links: Link[];
+  create_time: Date;
+  update_time: Date;
 }
 
 export interface SellerProtection {
-  status:             string;
+  status: string;
   dispute_categories: string[];
 }
 
 export interface SellerReceivableBreakdown {
   gross_amount: Amount;
-  paypal_fee:   Amount;
-  net_amount:   Amount;
+  paypal_fee: Amount;
+  net_amount: Amount;
 }
 
 export interface Shipping {
-  name:    ShippingName;
+  name: ShippingName;
   address: ShippingAddress;
 }
 
 export interface ShippingAddress {
   address_line_1: string;
-  admin_area_2:   string;
-  admin_area_1:   string;
-  postal_code:    string;
-  country_code:   string;
+  admin_area_2: string;
+  admin_area_1: string;
+  postal_code: string;
+  country_code: string;
 }
 
 export interface ShippingName {
