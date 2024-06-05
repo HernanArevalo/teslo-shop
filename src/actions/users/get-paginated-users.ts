0277ad895e25d2d527c6b323180eea0e5fc80d18
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-06-05 17:20:30 -0300

    add getPaginatedUsers action

---
+++ src/actions/users/get-paginated-users.ts
@@ -0,0 +1,29 @@
+'use server'
+
+import { auth } from "@/auth.config";
+import prisma from "@/lib/prisma";
+
+
+export const getPaginatedUsers = async() => {
+
+  const session = await auth();
+
+  if (session?.user.role !== 'admin') {
+    return {
+      ok: false,
+      message: "You're not an admin"
+    }
+  }
+
+  const users = await prisma.user.findMany({
+    orderBy: {
+      name: "desc"
+    }
+  })
+
+  return{
+    ok: true,
+    users: users
+  }
+
+}
