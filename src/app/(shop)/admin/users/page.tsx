7fb03c82d45610a7e88690d5bef2c2f48117a16b
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-06-05 17:20:55 -0300

    Add UsersPage

---
+++ src/app/(shop)/admin/users/page.tsx
@@ -0,0 +1,31 @@
+export const revalidate = 0;
+
+// https://tailwindcomponents.com/component/hoverable-table
+import { redirect } from 'next/navigation';
+
+import { getPaginatedUsers } from '@/actions';
+import { Pagination, Title } from '@/components';
+import { UsersTable } from './ui';
+
+
+export default async function UsersPage() {
+
+  const { ok, users = [] } = await getPaginatedUsers();
+
+
+  if ( !ok ) {
+    redirect('/auth/login')
+  }
+
+  return (
+    <>
+      <Title title="Users" />
+
+      <div className="mb-10">
+        <UsersTable users={users} />
+
+        <Pagination totalPages={3}/>
+      </div>
+    </>
+  );
+}
