8ca9044317f776ba7edf39ecf59c166720aa3133
Hernán Arévalo <hernanarevalo16@gmail.com>
2024-06-15 14:00:19 -0300

    add images upload

--- src/app/(shop)/admin/product/[slug]/ui/ProductForm.tsx
+++ src/app/(shop)/admin/product/[slug]/ui/ProductForm.tsx
@@ -1,70 +1,82 @@
+'use client';
 
-"use client";
-
-import { createUpdateProduct } from "@/actions";
-import { Category, Product, ProductImage, Size } from "@/interfaces";
-import clsx from "clsx";
-import Image from "next/image";
-import { useForm } from "react-hook-form";
+import { createUpdateProduct, deleteProductImage } from '@/actions';
+import { ProductImage } from '@/components';
+import { Category, Product, ProductImage as ProductImageInt } from '@/interfaces';
+import clsx from 'clsx';
+import { useRouter } from 'next/navigation';
+import { useForm } from 'react-hook-form';
+import Swal from 'sweetalert2';
 
 interface Props {
-  product: Partial<Product> & { ProductImage?: ProductImage[]};
+  product: Partial<Product> & { ProductImage?: ProductImageInt[] };
   categories: Category[];
 }
 
-const sizes = ["XS", "S", "M", "L", "XL", "XXL"];
+const sizes = ['XS', 'S', 'M', 'L', 'XL', 'XXL'];
 
 interface FormInputs {
   categoryId: string;
   description: string;
-  gender: 'men'|'women'|'kids'|'unisex';
+  gender: 'men' | 'women' | 'kids' | 'unisex';
   inStock: number;
   price: number;
   sizes: string[];
   slug: string;
   tags: string;
-  title: string; 
+  title: string;
 
   // TODO: images
+  images?: FileList;
+
 }
 
+export const ProductForm = ({ product, categories = [] }: Props) => {
 
-export const ProductForm = ({ product, categories=[] }: Props) => {
+  const router = useRouter();
 
-  const { 
+  const {
     handleSubmit,
     register,
     formState: { isValid },
     getValues,
     setValue,
-    watch
-
-   } = useForm<FormInputs>({
+    watch,
+  } = useForm<FormInputs>({
     defaultValues: {
       ...product,
       tags: product.tags?.join(', '),
       sizes: product.sizes ?? [],
 
       // TODO: images
-    }
-   });
+      images: undefined
+    },
+  });
 
-   watch('sizes');
+  watch('sizes');
 
-   const onSizeChanged = (size: string) => {
+  const onSizeChanged = (size: string) => {
+    const sizes = new Set(getValues('sizes'));
 
-    const sizes = new Set(getValues('sizes'))
+    sizes.has(size) ? sizes.delete(size) : sizes.add(size);
 
-    sizes.has(size) ? sizes.delete(size) : sizes.add(size)
-    
-    setValue('sizes', Array.from( sizes ));
-   }
+    setValue('sizes', Array.from(sizes));
+  };
 
-   const onSubmit = async(data: FormInputs ) => {
-    
+  const onSubmit = async (data: FormInputs) => {
     const formData = new FormData();
 
-    const { ...productToSave } = data
+    const { images, ...productToSave } = data;
+
+    if (productToSave.sizes.length == 0) {
+      Swal.fire({
+        title: 'Error!',
+        text: 'Select a Size',
+        icon: 'error',
+        confirmButtonText: 'Cool'
+      })
+      return
+    }
 
     if (product.id) {
       formData.append('id', product.id ?? '');
@@ -79,23 +91,45 @@ export const ProductForm = ({ product, categories=[] }: Props) => {
     formData.append('categoryId', productToSave.categoryId);
     formData.append('gender', productToSave.gender);
 
-    const { ok } = await createUpdateProduct(formData)
+    if (images) {
+      for( let i = 0; i < images.length; i++){
+        formData.append('images', images[i])
+      }
+    }
 
-   }
+    const { ok, product:updatedProduct } = await createUpdateProduct(formData);
 
+    if ( !ok ) {
+      alert('Error updating product')
+      return
+    };
+
+    router.replace(`/admin/product/${updatedProduct?.slug}`)
+  };
 
   return (
-    <form onSubmit={ handleSubmit(onSubmit) } className="grid px-5 mb-16 grid-cols-1 sm:px-0 sm:grid-cols-2 gap-3">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="grid px-5 mb-16 grid-cols-1 sm:px-0 sm:grid-cols-2 gap-3"
+    >
       {/* Textos */}
       <div className="w-full">
         <div className="flex flex-col mb-2">
           <span>Title</span>
-          <input type="text" className="p-2 border rounded-md bg-gray-200" { ...register('title', {required: true}) }/>
+          <input
+            type="text"
+            className="p-2 border rounded-md bg-gray-200"
+            {...register('title', { required: true })}
+          />
         </div>
 
         <div className="flex flex-col mb-2">
           <span>Slug</span>
-          <input type="text" className="p-2 border rounded-md bg-gray-200" { ...register('slug', {required: true}) }/>
+          <input
+            type="text"
+            className="p-2 border rounded-md bg-gray-200"
+            {...register('slug', { required: true })}
+          />
         </div>
 
         <div className="flex flex-col mb-2">
@@ -103,24 +137,31 @@ export const ProductForm = ({ product, categories=[] }: Props) => {
           <textarea
             rows={5}
             className="p-2 border rounded-md bg-gray-200"
-            { ...register('description', {required: true}) }
+            {...register('description', { required: true })}
           ></textarea>
         </div>
 
         <div className="flex flex-col mb-2">
           <span>Price</span>
-          <input type="number" className="p-2 border rounded-md bg-gray-200" { ...register('price', {required: true, min: 0}) }/>
+          <input
+            type="number"
+            className="p-2 border rounded-md bg-gray-200"
+            {...register('price', { required: true, min: 0 })}
+          />
         </div>
 
         <div className="flex flex-col mb-2">
           <span>Tags</span>
-          <input type="text" className="p-2 border rounded-md bg-gray-200" { ...register('tags', {required: true}) }/>
+          <input
+            type="text"
+            className="p-2 border rounded-md bg-gray-200"
+            {...register('tags', { required: true })}
+          />
         </div>
 
         <div className="flex flex-col mb-2">
           <span>Gender</span>
           <select className="p-2 border rounded-md bg-gray-200" { ...register('gender', {required: true}) }> 
-            <option hidden>[Select]</option>
             <option value="men">Men</option>
             <option value="women">Women</option>
             <option value="kid">Kid</option>
@@ -131,7 +172,6 @@ export const ProductForm = ({ product, categories=[] }: Props) => {
         <div className="flex flex-col mb-2">
           <span>Category</span>
           <select className="p-2 border rounded-md bg-gray-200" { ...register('categoryId', {required: true}) }>
-            <option hidden >[Select]</option>
             { categories.map(category => (
               <option value={category.id} key={category.id}>{category.name}</option>
             ))
@@ -139,86 +179,78 @@ export const ProductForm = ({ product, categories=[] }: Props) => {
           </select>
         </div>
 
-        <button className="btn-primary w-full">
-          Save
-        </button>
+        <button className="btn-primary w-full">Save</button>
       </div>
 
       {/* Selector de tallas y fotos */}
       <div className="w-full">
-
         <div className="flex flex-col mb-2">
           <span>Stock</span>
-          <input type="number" className="p-2 border rounded-md bg-gray-200" { ...register('inStock', {required: true, min: 0}) }/>
+          <input
+            type="number"
+            className="p-2 border rounded-md bg-gray-200"
+            {...register('inStock', { required: true, min: 0 })}
+          />
         </div>
 
         {/* As checkboxes */}
         <div className="flex flex-col gap-2">
-
           <div>
             <span>Sizes</span>
             <div className="flex flex-wrap">
-              
-              {
-                sizes.map( size => (
-                  // bg-blue-500 text-white <--- si está seleccionado
-                  <div key={ size }
-                       onClick={() => onSizeChanged(size)}
-                       className={clsx("p-2 cursor-pointer border rounded-md mr-2 mb-2 w-14 transition-all text-center",
-                                       { "bg-blue-500 text-white": getValues('sizes').includes(size)
-
-                                       }
-                       )}
-                       
-                       >
-                    <span>{ size }</span>
-                  </div>
-                ))
-              }
-
+              {sizes.map((size) => (
+                // bg-blue-500 text-white <--- si está seleccionado
+                <div
+                  key={size}
+                  onClick={() => onSizeChanged(size)}
+                  className={clsx(
+                    'p-2 cursor-pointer border rounded-md mr-2 mb-2 w-14 transition-all text-center',
+                    {
+                      'bg-blue-500 text-white':
+                        getValues('sizes').includes(size),
+                    }
+                  )}
+                >
+                  <span>{size}</span>
+                </div>
+              ))}
             </div>
-
           </div>
 
-
           <div className="flex flex-col mb-2">
-
             <span>Pictures</span>
-            <input 
+            <input
+              { ...register('images') }
               type="file"
-              multiple 
-              className="p-2 border rounded-md bg-gray-200" 
+              multiple
+              className="p-2 border rounded-md bg-gray-200"
               accept="image/png, image/jpeg"
             />
-
           </div>
 
           <div className="grid grid-cols-1 sm:grid-cols-3 gap-3">
-            { product.ProductImage?.map(image => (
-
+            {product.ProductImage?.map((image) => (
               <div key={image.id} className="">
-                <Image
-                  alt={ product.title ?? ''}
-                  src={`/products/${image.url}`}
+                <ProductImage
+                  alt={product.title ?? ''}
+                  src={image.url}
                   width={300}
                   height={300}
                   className="rounded-t shadow-md"
                 />
 
-                <button className="btn-danger w-full rounded-b-xl" 
-                        type="button"
-                        onClick={()=>{console.log(image.id, image.url);}}
-                        >
+                <button
+                  className="btn-danger w-full rounded-b-xl"
+                  type="button"
+                  onClick={() => {
+                    deleteProductImage(image.id, image.url);
+                  }}
+                >
                   Delete
                 </button>
               </div>
-
-            ))
-
-            }
+            ))}
           </div>
-
-
         </div>
       </div>
     </form>
