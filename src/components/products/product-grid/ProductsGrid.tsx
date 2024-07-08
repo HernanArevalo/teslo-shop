c7f36509d461c2c9d9cc3278c3d0711b959a591e
Hernán Arévalo <hernanarevalo16@gmail.com>
2024-07-08 14:40:07 -0300

    change grid-cols in phones

--- src/components/products/product-grid/ProductsGrid.tsx
+++ src/components/products/product-grid/ProductsGrid.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export const ProductsGrid = ( { products }: Props ) => {
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-3 gap-10 mb-10">
+    <div className="grid grid-cols-1 sm:grid-cols-3 gap-10 mb-10">
       {
         products.map( product => (
           <ProductsGridItem
