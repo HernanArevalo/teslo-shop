9087d8bf088b66adebaca522e7176f0e3ade3f39
Hernan Arevalo <hernanarevalo16@gmail.com>
2025-01-27 14:01:16 -0300

    optimize sidebar

--- src/components/ui/sidebar/Sidebar.tsx
+++ src/components/ui/sidebar/Sidebar.tsx
@@ -16,126 +16,127 @@ import {
 import { useUiStore } from '@/store';
 import { logout } from '@/actions';
 import { useSession } from 'next-auth/react';
+import React from 'react';
 
 export const Sidebar = () => {
-  const {isSideMenuOpen, closeSideMenu } = useUiStore();
+  const { isSideMenuOpen, closeSideMenu } = useUiStore();
 
   const { data: session } = useSession();
-  const isAuthenticated = !!session?.user
-  const isAdmin = (session?.user.role === 'admin');
-
+  const isAuthenticated = !!session?.user;
+  const isAdmin = session?.user.role === 'admin';
 
   return (
-    <div className="">
-      {/* black background */}
-      {isSideMenuOpen && (
-        <div className="fixed top-0 left-0 w-screen h-screen z-10 bg-black opacity-30  backdrop-filter backdrop-blur-xl" />
-      )}
-
-      {/* blur */}
+    <div>
+      {/* Fondo negro semitransparente con evento onClick */}
       {isSideMenuOpen && (
         <div
-          className="fade-in fixed top-0 left-0 w-screen h-screen z-10 backdrop-filter backdrop-blur-xl"
+          className="fixed top-0 left-0 w-screen h-screen z-10 bg-black/50"
           onClick={closeSideMenu}
         />
       )}
 
-      {/* sidemenu */}
+      {/* Sidebar */}
       <nav
-        // TODO: slide effect
         className={clsx(
-          'fixed p-5 right-0 top-0 max-w-screen-sm sm:w-[500px] h-screen bg-white z-20 shadow-2xl transform transition-all duration-300',
+          'fixed p-5 right-0 top-0 max-w-screen-sm sm:w-[500px] h-screen bg-white z-20 shadow-2xl transform transition-transform duration-300 will-change-transform',
           {
             'translate-x-full': !isSideMenuOpen,
           }
         )}
       >
+        {/* Botón de cierre */}
         <IoCloseCircleOutline
           size={50}
           className="absolute top-5 right-5 cursor-pointer"
           onClick={closeSideMenu}
         />
 
-        {/* Input */}
-        <div className="relative mt-14">
-          <IoSearchOutline size={20} className="absolute top-2 left-2" />
-          <input
-            type="text"
-            className="w-full bg-gray-50 rounded pl-10 py-1 pr-10 border-b-2 text-xl border-gray-200 focus:outline-none focus:border-blue-500"
-          />
-        </div>
+        {/* Input de búsqueda */}
+        {isSideMenuOpen && (
+          <div className="relative mt-14">
+            <IoSearchOutline size={20} className="absolute top-2 left-2" />
+            <input
+              type="text"
+              className="w-full bg-gray-50 rounded pl-10 py-1 pr-10 border-b-2 text-xl border-gray-200 focus:outline-none focus:border-blue-500"
+            />
+          </div>
+        )}
 
-        {/* Menu */}
-        { isAuthenticated && (
+        {/* Menú principal */}
+        {isSideMenuOpen && (
           <>
-            <Link
-              href={'/profile'}
-              className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all gap-3"
-              onClick={closeSideMenu}
-            >
-              <IoPersonOutline size={30} />
-              <span className="text-xl">Profile</span>
-            </Link>
+            {isAuthenticated && (
+              <>
+                <Link
+                  href={'/profile'}
+                  className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all gap-3"
+                  onClick={closeSideMenu}
+                >
+                  <IoPersonOutline size={30} />
+                  <span className="text-xl">Profile</span>
+                </Link>
 
-            <Link
-              href={'/orders'}
-              className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all gap-3"
-              onClick={closeSideMenu}
-            >
-              <IoTicketOutline size={30} />
-              <span className="text-xl">My orders</span>
-            </Link>
+                <Link
+                  href={'/orders'}
+                  className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all gap-3"
+                  onClick={closeSideMenu}
+                >
+                  <IoTicketOutline size={30} />
+                  <span className="text-xl">My orders</span>
+                </Link>
+              </>
+            )}
+            {isAuthenticated ? (
+              <button
+                className="flex items-center w-full mt-10 p-2 hover:bg-gray-100 rounded transition-all gap-3"
+                onClick={() => logout()}
+              >
+                <IoLogOutOutline size={30} />
+                <span className="text-xl">Log-out</span>
+              </button>
+            ) : (
+              <Link
+                href={'/auth/login'}
+                className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all gap-3"
+                onClick={closeSideMenu}
+              >
+                <IoLogInOutline size={30} />
+                <span className="text-xl">Log-in</span>
+              </Link>
+            )}
+            {isAdmin && (
+              <>
+                <div className="w-full h-px bg-gray-200 my-10" />
+
+                <Link
+                  href="/admin/products"
+                  className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all gap-3"
+                  onClick={closeSideMenu}
+                >
+                  <IoShirtOutline size={30} />
+                  <span className="text-xl">Products</span>
+                </Link>
+                <Link
+                  href="/admin/orders"
+                  className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all gap-3"
+                  onClick={closeSideMenu}
+                >
+                  <IoTicketOutline size={30} />
+                  <span className="text-xl">Orders</span>
+                </Link>
+                <Link
+                  href="/admin/users"
+                  className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all gap-3"
+                  onClick={closeSideMenu}
+                >
+                  <IoPeopleOutline size={30} />
+                  <span className="text-xl">Users</span>
+                </Link>
+              </>
+            )}
           </>
         )}
-        {isAuthenticated ? (
-          <button
-            className="flex items-center w-full mt-10 p-2 hover:bg-gray-100 rounded transition-all gap-3"
-            onClick={() => logout()}
-          >
-            <IoLogOutOutline size={30} />
-            <span className="text-xl">Log-out</span>
-          </button>
-        ) : (
-          <Link
-            href={'/auth/login'}
-            className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all gap-3"
-            onClick={closeSideMenu}
-            >
-            <IoLogInOutline size={30} />
-            <span className="text-xl">Log-in</span>
-          </Link>
-        )}
-        { isAdmin &&
-          <>
-            <div className="w-full h-px bg-gray-200 my-10" />
-
-            <Link
-              href="/admin/products"
-              className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all gap-3"
-              onClick={closeSideMenu}
-              >
-              <IoShirtOutline size={30} />
-              <span className="text-xl">Products</span>
-            </Link>
-            <Link
-              href="/admin/orders"
-              className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all gap-3"
-              onClick={closeSideMenu}
-              >
-              <IoTicketOutline size={30} />
-              <span className="text-xl">Orders</span>
-            </Link>
-            <Link
-              href="/admin/users"
-              className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all gap-3"
-              onClick={closeSideMenu}
-            >
-              <IoPeopleOutline size={30} />
-              <span className="text-xl">Users</span>
-            </Link>
-          </>
-        }
       </nav>
     </div>
   );
-};
+}
